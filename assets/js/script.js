52d5ad60457bc8b906eb3c7e30b8a9628c4977ba
debbie-herridge <debbie.herridge@icloud.com>
2022-10-05 10:17:33 +0000

    attempt to fix 501 error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,33 @@
 
-// get name and progress to quiz
-function getName(event){
-    event.preventDefault();
+// // get name and progress to quiz
+// function getName(event){
+//     event.preventDefault();
 
-    let userName = document.getElementById('name');
-    console.log(userName)
+//     let userName = document.getElementById('name');
+//     console.log(userName)
 
-    window.location.href="../../quiz.html";
+//     window.location.href="../../quiz.html";
     
-    let addUsersName = document.getElementById('name-result')
-    addUsersName.innerHTML = `<h2>${userName}</h2>`
-}
+//     let addUsersName = document.getElementById('name-result')
+//     addUsersName.innerHTML = `<h2>${userName}</h2>`
+// }
 
-// load general knowledge page to start that quiz
+// // load general knowledge page to start that quiz
 
-function startGeneral(event) {
-    window.onload = sendApiRequest
-    async function sendApiRequest(){
-        let response = await fetch(`https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple`);
-        console.log(response)
+// function startGeneral(event) {
+//     window.onload = sendApiRequest
+//     async function sendApiRequest(){
+//         let response = await fetch(`https://opentdb.com/api.php?amount=1&category=9&difficulty=easy&type=multiple`);
+//         console.log(response)
         
-    }
+//     }
 
-}
+// }
+
+
+let user = document.getElementById('username');
+let pass = document.getElementById('password');
+
+// Now you can get their values
+console.log(user.value);
+console.log(pass.value);
